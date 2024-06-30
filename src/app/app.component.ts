f891e8ee7bbf71a58060cafa921102c18b0a5fb8
TharushaKula <tharusha@purple.lk>
2024-06-30 22:34:50 +0530

    adding all the files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from './components/footer/footer.component';
+import {WelcomeComponent} from './components/welcome/welcome.component'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, WelcomeComponent]
 })
 export class AppComponent {
   title = 'newsy';
