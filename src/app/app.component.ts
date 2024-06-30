13b231a8c12b55e085b65364b3c12b0d90bb7bdb
TharushaKula <tharusha@purple.lk>
2024-06-30 23:17:13 +0530

    Add main article component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from './components/footer/footer.component';
-import {WelcomeComponent} from './components/welcome/welcome.component'
+import {WelcomeComponent} from './components/welcome/welcome.component';
+import {MainArticleComponent} from './components/main-article/main-article.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, WelcomeComponent]
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, WelcomeComponent, MainArticleComponent]
 })
 export class AppComponent {
   title = 'newsy';
